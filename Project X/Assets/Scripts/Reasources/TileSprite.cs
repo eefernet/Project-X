3a34794f09adcb8184533eac7de2cbe7e3242de8
TacoCatDevTeam <ethanryanjamieson@gmail.com>
2016-12-19 18:42:25 -0700

    added tile engine for landscape

---
+++ Project X/Assets/Scripts/Reasources/TileSprite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[Serializable]
+public class TileSprite : MonoBehaviour {
+
+	public string Name;
+	public Sprite TileImage;
+	public Resources TileType;
+
+	public TileSprite(){
+		Name = "unset";
+		TileImage = new Sprite ();
+		TileType = Resources.unset;
+	}
+
+	public TileSprite(string name, Sprite tileImage, Resources tile){
+		Name = name;
+		TileImage = tileImage;
+		TileType = tile;
+	}
+
+}
---
+++ Project X/Assets/Scripts/Reasources/Tiles.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Resources {unset, iron, gold, silver, cobalt, nickel, titanium, magnesium, sodium, phosphorus, water, oxygen, nitrogen};
+
